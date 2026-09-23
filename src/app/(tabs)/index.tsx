fea22ffe23417e9f0f6c51b8ebf316a4cf47a7bc
RukiyeErtem <rukiyeertem939@gmail.com>
2026-09-23 18:07:13 +0300

    Show Trending Movies
    react native masked view npm added

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,23 @@
-import {ActivityIndicator, FlatList, Image, ScrollView, StyleSheet, Text, View} from "react-native";
-import {Link} from "expo-router";
+import {ActivityIndicator, FlatList, Image, ScrollView, Text, View} from "react-native";
+import {useRouter} from "expo-router";
 import {images} from "../../../constants/images";
 import {icons} from "../../../constants/icons";
 import SearchBar from "../../components/searchbar";
-import {useRouter} from "expo-router";
 import fetchMovies from "@/services/api";
 import useFetch from "@/services/useFetch";
 import MovieCard from "@/components/MovieCard";
+import {getTrendingMovies} from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
     const router = useRouter();
+
+
+    const {
+        data: trendingMovies,
+        loading: trendingLoading,
+        error: trendingError,
+    } = useFetch(getTrendingMovies);
 
     const {
         data: movies,
@@ -23,61 +31,66 @@ export default function Index() {
     return (
         <View className="flex-1 bg-primary">
             <Image source={images.bg} className={"absolute w-full z-0"}/>
-            <ScrollView className={"flex-1 px-5"} showsVerticalScrollIndicator={false} contentContainerStyle={{
-                minHeight: "100%",
-                paddingBottom: 10
-            }}>
-                <Image source={icons.logo} className={"w-12 h-10 mt-20 mb-5 mx-auto "}/>
+            {moviesLoading || trendingLoading ? (
+                <ActivityIndicator
+                    size={"large"}
+                    color={"#0000ff"}
+                    className={"mt-10 self-center"}/>
+            ) : moviesError || trendingError ? (
+                <Text className="px-5">
+                    Error: {moviesError?.message || trendingError?.message}
+                </Text>
+            ) : (
+                <FlatList
+                    className="flex-1 px-5"
+                    showsVerticalScrollIndicator={false}
+                    data={movies ?? []}
+                    renderItem={({item}) => <MovieCard {...item}/>}
+                    keyExtractor={(item) => item.id.toString()}
+                    numColumns={3}
+                    columnWrapperStyle={{
+                        justifyContent: "flex-start",
+                        gap: 20,
+                        paddingRight: 5,
+                        marginBottom: 10,
+                    }}
+                    contentContainerStyle={{paddingBottom: 100}}
+                    ListHeaderComponent={
+                        <View>
+                            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+                            <SearchBar
+                                onPress={() => router.push("/search")}
+                                placeholder="Search through 300+ movies online"
+                            />
 
-                {moviesLoading ? (
-                    <ActivityIndicator
-                        size={"large"}
-                        color={"#0000ff"}
-                        className={"mt-10 self-center"}/>
-                ) : moviesError ? (
-                    <Text>Error: {moviesError?.message}</Text>
-                ) : (
-                    <View className={"flex-1 mt-5"}>
-                        <SearchBar
-                            onPress={() => router.push("/search")}
-                            placeholder={"Search for a movie"}
-                        />
+                            {trendingMovies && trendingMovies.length > 0 && (
+                                <View className="mt-10">
+                                    <Text className="text-lg text-white font-bold mb-3">
+                                        Trending Movies
+                                    </Text>
+                                    <ScrollView
+                                        horizontal
+                                        showsHorizontalScrollIndicator={false}
+                                        contentContainerStyle={{gap: 26}}
+                                    >
+                                        {trendingMovies.map((item, index) => (
+                                            <TrendingCard
+                                                key={`${item.movie_id}-${index}`}
+                                                movie={item}
+                                                index={index}
+                                            />
+                                        ))}
+                                    </ScrollView>
+                                </View>
+                            )}
 
-                        <>
-                            <Text className={"text-lg text-white font-bold mb-3 mt-5"}>
+                            <Text className="text-lg text-white font-bold mb-3 mt-5">
                                 Latest Movies
                             </Text>
-
-                            <FlatList
-                                data={movies}
-                                renderItem={({item}) => (
-                                    <MovieCard {...item}/>
-                                )}
-                                keyExtractor={(item) => item.id.toString()}
-                                numColumns={3}
-                                columnWrapperStyle={{
-                                    justifyContent: 'flex-start',
-                                    gap: 20,
-                                    paddingRight: 5,
-                                    marginBottom: 10
-                                }}
-                                className={"mt-2 pb-32"}
-                                scrollEnabled={false}
-                            />
-                        </>
-                    </View>
-                )}
-
-
-            </ScrollView>
+                        </View>
+                    }
+                />
+            )}
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
